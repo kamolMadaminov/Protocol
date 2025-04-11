52a95ffe6ae3bd9646205f4015bcca3ebee7470e
developerkamol <developerkamol@gmail.com>
2025-04-11 20:25:00 +0500

    Initial Commit

---
+++ Protocol/ProtocolApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProtocolApp.swift
+//  Protocol
+//
+//  Created by Kamol Madaminov on 11/04/25.
+//
+
+import SwiftUI
+
+@main
+struct ProtocolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
